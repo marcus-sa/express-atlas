509972de18a4c60dcd86c276a28dfd6963ce55d3
Marcus S. Abildskov <esetmarcussharkgamingcomputer@gmail.com>
2017-05-02 21:42:50 +0200

    Updates

--- test/test.js
+++ test/test.js
@@ -1,3 +1,12 @@
+/*
+ * atlas
+ * https://github.com/marcus-sa/express-atlas
+ *
+ * Copyright (c) 2016 Marcus S. Abildskov
+ * Licensed under the MIT license.
+ */
+
+
 import express from 'express'
 import AtlasRouter from '../src'
 import path from 'path'
